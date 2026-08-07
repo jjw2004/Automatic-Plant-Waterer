f767d29397407e08befa1b7497f1a3316d6036bd
jjw2004 <wrightjoey2004@gmail.com>
2026-08-07 16:12:10 +0100

    plant monitor V2 with oled screen instead of LEDS

---
+++ plant-monitor-oled.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SOIL_PIN 34
+#define OLED_ADDR 0x3C
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+}
+
+void loop() {
+
+  // Read soil moisture sensor
+  int moisture = analogRead(SOIL_PIN);
+
+  // Convert to percentage
+  int moisturePercent = map(moisture, 4095, 0, 0, 100);
+  moisturePercent = constrain(moisturePercent, 0, 100);
+
+  // Print to Serial
+  Serial.print("Raw Value: ");
+  Serial.print(moisture);
+  Serial.print("  Moisture: ");
+  Serial.print(moisturePercent);
+  Serial.println("%");
+
+  // Update OLED
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  if (moisturePercent < 30) {
+    display.println("DRY!");
+  }
+  else if (moisturePercent < 70) {
+    display.println("GOOD");
+  }
+  else {
+    display.println("WET");
+  }
+
+  display.print(moisturePercent);
+  display.println("%");
+
+  display.display();
+
+  delay(2000);
+}
